d352583d9b1e055b8da8eba670fbc4a5c7c3267b
Jessica Pope <jessica.pope.co@gmail.com>
2019-04-15 17:39:17 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 2
    array.join(" and ")
  elsif array.length >2
-   array[2] = "and"+ array[2]
+   array[-1] = "and"+ array[-1]
    array.join(", ")
 end
 end
